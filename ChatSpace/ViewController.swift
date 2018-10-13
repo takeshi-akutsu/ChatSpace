71e63e454e7552c53cc9b8a25ac15fd740724741
Takeshi Akutsu <takeshi.akutsu0303+gitlab@gmail.com>
2018-10-13 15:33:16 +0900

    initial commit

---
+++ ChatSpace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatSpace
+//
+//  Created by Takeshi Akutsu on 2018/10/13.
+//  Copyright © 2018年 Takeshi Akutsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
